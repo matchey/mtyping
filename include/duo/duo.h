3dcc4c3825f61f174dd705421dc7d34704d85720
matchey <19305109+matchey@users.noreply.github.com>
2018-05-10 11:12:08 +0900

    add

--- include/duo/duo.h
+++ include/duo/duo.h
@@ -2,9 +2,25 @@
 #ifndef DUO_H
 #define DUO_H
 
+#include <iostream>
+#include <vector>
+
+struct Sentense
+{
+	std::string eng;
+	std::string jpn;
+};
+
 class Duo
 {
+	std::vector<Sentense> sentenses;
 
+	void load(const std::string&);
+
+	public:
+	Duo(const std::string&);
+	void print();
+	void typing();
 };
 
 #endif
--- src/duo/duo.cpp
+++ src/duo/duo.cpp
@@ -1,7 +1,31 @@
 
 #include "duo/duo.h"
 
-Duo::Duo()
+using namespace std;
+
+Duo::Duo(const string& filename)
 {
+}
+
+void Duo::print()
+{
+}
+
+void Duo::typing()
+{
+}
+
+// private
+void Duo::load(const string& filename)
+{
+	// section
+	// 1,2,3,
+	// 1~3
+	// random 3
+	// other random 3
+	//
+	// time (default 1 min)
+	// 
+	// other sentenses
 }
 
